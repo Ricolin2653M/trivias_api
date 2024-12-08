f0c440c26f84a612232980d33824c1492edcb5ae
Ricolin2653M <car6920.03.03@gmail.com>
2024-12-08 15:30:04 -0600

    first commit

---
+++ src/routes/resultados.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import * as resultadoCtrl from "../controllers/resultados.controller.js";
+
+const router = Router();
+
+router.post("/", resultadoCtrl.createResultado);
+router.get("/", resultadoCtrl.getResultados);
+router.get("/:resultadoId", resultadoCtrl.getResultadoById);
+router.get("/trivia/:triviaId", resultadoCtrl.getResultadosPorTrivia);
+router.get("/promedio/usuario/:idUsuario", resultadoCtrl.obtenerPromedioUsuario);
+router.get("/puntaje/usuario/:idUsuario", resultadoCtrl.obtenerPuntajeUsuario);
+router.get("/historial/usuario/:idUsuario", resultadoCtrl.obtenerHistorialUsuario)
+router.get("/top/promedio/:idTrivia", resultadoCtrl.obtenerTopPromedio);
+router.get("/top/puntaje/:idTrivia", resultadoCtrl.obtenerTopPuntaje);
+
+export default router;
